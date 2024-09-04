d07f05e3aa0025ee78a11110b5b2386013beace3
Leobardo R Rountree <leobardo.r.rountree@intel.com>
2024-09-04 16:23:33 -0700

    Start adding clients for database and config

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/lrrountr/racing-draft-api
 go 1.22.3
 
 require (
+	github.com/gin-contrib/cors v1.7.2
 	github.com/gin-gonic/gin v1.10.0
 	github.com/google/uuid v1.6.0
 	github.com/jackc/pgx/v4 v4.7.1
@@ -42,7 +43,7 @@ require (
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.7 // indirect
 	github.com/konsorten/go-windows-terminal-sequences v1.0.2 // indirect
-	github.com/kr/pretty v0.3.0 // indirect
+	github.com/kr/text v0.2.0 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mitchellh/copystructure v1.0.0 // indirect
@@ -50,7 +51,6 @@ require (
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.2 // indirect
-	github.com/rogpeppe/go-internal v1.8.0 // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.12 // indirect
@@ -62,6 +62,5 @@ require (
 	golang.org/x/text v0.15.0 // indirect
 	golang.org/x/xerrors v0.0.0-20191204190536-9bdfabe68543 // indirect
 	google.golang.org/protobuf v1.34.1 // indirect
-	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- internal/model/lib.go
+++ internal/model/lib.go
@@ -2,10 +2,18 @@ package model
 
 import (
 	"context"
+	"crypto/x509"
+	"errors"
 	"fmt"
+	"io"
+	"net/http"
+	"time"
 
 	"github.com/jackc/pgx/v4"
 	"github.com/jackc/pgx/v4/pgxpool"
+	"github.com/jackc/tern/migrate"
+	"github.com/lrrountr/racing-draft-api/internal/config"
+	"github.com/lrrountr/racing-draft-api/internal/model/migrations"
 )
 
 type DBClient struct {
@@ -18,4 +26,92 @@ func (c DBClient) Begin(ctx context.Context) (tx pgx.Tx, err error) {
 		return nil, fmt.Errorf("failed to start TX: %w", err)
 	}
 	return tx, nil
+}
+
+func NewClient(conf config.Config) (client DBClient, err error) {
+	ctx, cls := context.WithTimeout(context.Background(), 5*time.Minute)
+	defer cls()
+
+	dbConfig := conf.DB
+	tlsMode := "prefer"
+	var rootCAs *x509.CertPool = nil
+	if conf.DB.Region != "" {
+		rootCAs, err = setupTLSPEMBundle(dbConfig)
+		if err != nil {
+			return client, err
+		}
+		tlsMode = "verify-full"
+	}
+
+	dbConf, err := pgxpool.ParseConfig(
+		fmt.Sprintf(
+			"postgtresql://%s:%s@%s:%d/%s?sslmode=%s",
+			dbConfig.User,
+			dbConfig.Password,
+			dbConfig.Host,
+			dbConfig.Port,
+			dbConfig.DBName,
+			tlsMode,
+		),
+	)
+	if err != nil {
+		return client, fmt.Errorf("could not parse config: %w", err)
+	}
+	if rootCAs != nil {
+		dbConf.ConnConfig.TLSConfig.RootCAs = rootCAs
+	}
+
+	pool, err := pgxpool.ConnectConfig(ctx, dbConf)
+	if err != nil {
+		return client, fmt.Errorf("could not connect to database: %w", err)
+	}
+	client = DBClient{
+		pool: pool,
+	}
+	p, err := client.pool.Acquire(ctx)
+	if err != nil {
+		return client, err
+	}
+	defer p.Release()
+
+	conn := p.Conn()
+	ms, err := migrate.NewMigrator(ctx, conn, migrations.MigrationsTable)
+	if err != nil {
+		return client, fmt.Errorf("could not create migratior: %w", err)
+	}
+
+	ms = migrations.Init(ms)
+	err = ms.Migrate(ctx)
+	if err != nil {
+		return client, fmt.Errorf("could not migrate table: %w", err)
+	}
+
+	return client, nil
+}
+
+func setupTLSPEMBundle(conf config.DBConf) (*x509.CertPool, error) {
+	url := fmt.Sprintf("https://truststore.pki.rds.amazonaws.com/%s/%s-bundle.pem", conf.Region, conf.Region)
+	c := &http.Client{
+		Timeout: 10 * time.Minute,
+	}
+	resp, err := c.Get(url)
+	if err != nil {
+		return nil, fmt.Errorf("could not get TLS PEM bundle: %w", err)
+	}
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("invalid status code downloading PEM bundle: %d", resp.StatusCode)
+	}
+	defer resp.Body.Close()
+
+	chain, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	rootCAs := x509.NewCertPool()
+	ok := rootCAs.AppendCertsFromPEM(chain)
+	if !ok {
+		return nil, errors.New("AWS returned invalid Cert PEMs")
+	}
+	return rootCAs, nil
 }
