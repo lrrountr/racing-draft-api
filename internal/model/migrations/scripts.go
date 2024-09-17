a9704a1cc9ce32bafb5b5d5037791b0adb338e51
Leobardo R Rountree <leobardo.r.rountree@intel.com>
2024-09-17 14:01:56 -0700

    Add users and fetching them, model code

--- internal/model/migrations/scripts.go
+++ internal/model/migrations/scripts.go
@@ -1,6 +1,17 @@
 package migrations
 
 const (
+	CreatePGExtensionsUp = `
+		CREATE EXTENSION pg_trgm;
+		CREATE EXTENSION fuzzystrmatch;
+	`
+	CreatePGExtensionsDown = `
+		DROP EXTENSION pg_trgm;
+		DROP EXTENSION fuzzystrmatch;
+	`
+)
+
+const (
 	SeasonsTableUp = `
 		BEGIN;
 			CREATE TABLE seasons(
@@ -17,6 +28,27 @@ const (
 		BEGIN;
 			DROP INDEX unique_season_uuid;
 			DROP TABLE seasons;
+		COMMIT;
+	`
+)
+
+const (
+	UsersTableUp = `
+		BEGIN;
+			CREATE TABLE users(
+				uuid TEXT NOT NULL,
+				name TEXT NOT NULL,
+				email TEXT NOT NULL
+			);
+			CREATE UNIQUE INDEX unique_user_email ON users(email);
+			CREATE INDEX users_search_terms_idx ON users USING GIN(to_tsvector('english', name || ' ' || email));
+		COMMIT;
+	`
+	UsersTableDown = `
+		BEGIN;
+			DROP INDEX unique_user_email;
+			DROP INDEX users_search_terms_idx;
+			DROP TABLE users;
 		COMMIT;
 	`
 )
